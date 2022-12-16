68029a257228c49f42c3ea224b1b7603974a75bc
gianca-03 <gianca.ruiz@hotmail.com>
2022-12-16 16:51:38 -0600

    Initial commit

---
+++ app/src/main/java/com/example/proyectofinal/Principal.kt
@@ -0,0 +1,59 @@
+package com.example.proyectofinal
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.navigation.NavigationView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.appcompat.app.AppCompatActivity
+import com.example.proyectofinal.databinding.ActivityPrincipalBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class Principal : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityPrincipalBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityPrincipalBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val navController = findNavController(R.id.nav_host_fragment_content_principal)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.principal, menu)
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_principal)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId){
+            R.id.action_logout -> {
+                Firebase.auth.signOut()
+                finish()
+                true
+            } else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
